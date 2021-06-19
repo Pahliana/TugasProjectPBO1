d562310b491bdb72f2254c455842aa510b805198
pahliana <phliana007@gmail.com>
2021-06-19 13:37:03 +0800

    Membuat 8 atribut dan 4 Method di Class Hewan

---
+++ src/model/hewan.java
@@ -0,0 +1,27 @@
+package model;
+
+public class hewan {
+
+    // 8 atribut class hewan
+
+    String namaHewan;
+    String panggilanHewan;
+    String jenisHewan;
+    String jenisKelamin;
+    String warnaHewan;
+    String makananHewan;
+    int jumlahKaki;
+    int umurHewan;
+
+    // 4 Method class hewan
+
+    public void tampilkanAtribut(){}
+    public void tampilkanInfoHewan(){}
+    public boolean create(){
+        return false;
+    }
+    public boolean delete(){
+        return false;
+    }
+
+}
